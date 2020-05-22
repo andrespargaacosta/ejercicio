37ae63f91386890519166654d54c17eb219c9a54
Andrés <aparga@innovaweb.cl>
2020-05-22 16:57:58 -0400

    hotfix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,7 +51,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        /*
+        
         if($exception instanceof NotFoundHttpException){
             return response(
                 json_encode(array('error'=>"Resource not found")),404
@@ -61,7 +61,7 @@ class Handler extends ExceptionHandler
         if(method_exists($exception,'getStatusCode') && $exception->getStatusCode() == 405);{
             return response(json_encode(array( 'error'=>'Method not allowed' )),405)->header('Content-Type','application/json');
         }
-        */
+        
         
         return parent::render($request, $exception);
     }
